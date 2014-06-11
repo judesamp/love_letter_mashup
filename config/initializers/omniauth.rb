ac9efe4f7c4f075e81b6f259f9ee02e213829352
Jeremy Samples <judesamp@gmail.com>
2014-06-11 09:14:47 -0400

    fiddling with environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret
-  provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret, {
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], {
   client_options: { ssl: { 
         ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
         ca_path: "/etc/ssl/certs"
