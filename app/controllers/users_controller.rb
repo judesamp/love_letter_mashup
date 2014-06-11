4397a5f466d4c941dde5ee0809b5c4a344259d45
Jeremy Samples <judesamp@gmail.com>
2014-06-10 20:09:23 -0400

    efforting correct config for custom gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to letters_path, notice: "You've signed up and we've logged you in. Now go mash some letters."
+      redirect_to new_letter_path, notice: "You've signed up and we've logged you in. Now go mash some letters."
     else
-      redirect_to letters_path, notice: "We were unable to create your account. Please try again."
+      redirect_to welcome_path, notice: "We were unable to create your account. Please try again."
     end
   end
 
