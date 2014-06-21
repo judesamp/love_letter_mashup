72cc5ffe89c67ee1cc1fc66566d9fce5fad11985
Jeremy Samples <judesamp@gmail.com>
2014-06-21 15:53:55 -0400

    working authorization issue with pdf

--- app/controllers/letter_orders_controller.rb
+++ app/controllers/letter_orders_controller.rb
@@ -77,11 +77,10 @@ class LetterOrdersController < ApplicationController
     @letter_order = letter_order
     @letter = Letter.find(@letter_order.letter_id)
     user = User.find(@letter_order.user_id)
-    Lob.configure do | config |
+    @log = Lob.configure do | config |
       config.protocol = "http"
       config.api_key = "test_54d506bcb9685853d7189ac266b7e173a1e"
     end
-    @lob = Lob(api_key: "test_54d506bcb9685853d7189ac266b7e173a1e")
     response = @lob.jobs.create(
       name: "Inline Test Job",
       from: {
