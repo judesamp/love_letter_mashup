d1c01f558a80228108c82fba3cbfc9bc7e32c338
Jeremy Samples <judesamp@gmail.com>
2014-08-08 11:20:14 -0400

    bug fix in snippet model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,9 @@
 class Snippet < ActiveRecord::Base
 	belongs_to :author
 	has_many :letter_snippets
-	has_many :letters, through: :letter_snippets	
+	has_many :letters, through: :letter_snippets
+
+
+	scope :by_position, -> { order('position asc') }
+	
 end
